8db478d0a420dc17fbaa658ea88af45d066afce7
Igor Flint <netwamp@gmail.com>
2024-04-05 06:14:54 +0700

    Add address confirmation and auth string recievment

--- synapse/signature_auth/auth.py
+++ synapse/signature_auth/auth.py
@@ -1,5 +1,7 @@
 import logging
+import datetime as dt
 import time
+import re
 
 from eth_account import Account
 from eth_account.messages import encode_defunct
@@ -17,7 +19,7 @@ class SignatureAuth:
         self.config = config
         account_handler.register_password_auth_provider_callbacks(
             auth_checkers={
-                ("login.signature", ("signature",)): self.check_my_login,
+                ("login.signature", ("signature", "message")): self.check_my_login,
             },
         )
 
@@ -32,12 +34,25 @@ class SignatureAuth:
         login_dict: "synapse.module_api.JsonDict",
         *args, **kwargs
     ):
-        message_encoded = encode_defunct(text=self.config["message"])
-        signature = login_dict["signature"]
-        signer_address = Account.recover_message(message_encoded, signature=signature).lower()
-        username = self.api.get_qualified_user_id(signer_address)
-        users = await self.api._hs.get_registration_handler().store.get_users_by_id_case_insensitive(username)
-        if not users:
-            await self.api.register_user(signer_address)
+        try:
+            message = login_dict["message"]
+            provided_address = username.lower()
+            signature_ttl = self.config["signature_ttl"]
+            message_encoded = encode_defunct(text=message)
+            signature = login_dict["signature"]
+            timestamp = int(message.split("Timestamp:")[1])
+            if int(dt.datetime.now().timestamp()) - timestamp > signature_ttl:
+                log.info(f"Timestamp too old, {int(dt.datetime.now().timestamp()) - timestamp}" )
+                return None
+            signer_address = Account.recover_message(message_encoded, signature=signature).lower()
+            if signer_address != provided_address:
+                log.info(f"Wrong address, potential scum: {provided_address}, {signer_address}")
+                return None
+            username = self.api.get_qualified_user_id(signer_address)
+            users = await self.api._hs.get_registration_handler().store.get_users_by_id_case_insensitive(username)
+            if not users:
+                await self.api.register_user(signer_address)
+        except Exception as e:
+            log.exception("Authentication via Ethereum signature failed")
         return (username, None)
 
